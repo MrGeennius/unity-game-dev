8e4be5a879b69859612b79be485bf0547841afe4
MrGeennius <mr.geennius@gmail.com>
2023-07-31 06:39:49 -0300

    proyectil,enemigo,camara,movimiento

--- opus-jewel/Assets/Scripts/personaje.cs
+++ opus-jewel/Assets/Scripts/personaje.cs
@@ -6,6 +6,13 @@ public class personaje : MonoBehaviour
 {
      public float velocidadMovimiento = 1f;
      private Rigidbody2D rb;
+     public GameObject Bala;
+     private GameObject bala;
+     private Transform Posicion; 
+     public Transform PuntoDisparo;
+
+     public float veldisparo=1f;
+     public float tiempoDisparo=1f, proximoDisparo=1f;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,15 +22,25 @@ public class personaje : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        // INICIO MOVIMIENTO JUGADOR
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        Vector2 movement = new Vector2(horizontalInput, verticalInput) * velocidadMovimiento * Time.deltaTime;
+        transform.Translate(movement);
+        // FIN MOVIMIENTO JUGADOR
+        // DISPARO
+        if (Input.GetMouseButtonDown(0) && Time.time>proximoDisparo){
+            Disparo();
+            proximoDisparo=Time.time+tiempoDisparo;
+        }
+        //FIN DISPARO
     }
 
+    void Disparo(){
+            bala = Instantiate(Bala, PuntoDisparo.position, Quaternion.identity);
+            bala.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,1)*veldisparo);
+        }
+
     private void FixedUpdate(){
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
-
-        Vector2 movement = new Vector2(horizontalInput, verticalInput) * velocidadMovimiento * Time.deltaTime;
-
-        transform.Translate(movement);
     }
 }
