61be9f93200d801d35c5320573812aeff8078d36
MrGeennius <mr.geennius@gmail.com>
2023-08-01 10:16:15 -0300

    movimiento enemigo

--- opus-jewel/Assets/Scripts/Enemigo.cs
+++ opus-jewel/Assets/Scripts/Enemigo.cs
@@ -2,16 +2,29 @@ using UnityEngine;
 
 public class Enemigo : MonoBehaviour
 {
-    public int vidasMaximas = 3;
+    [Header("Estadisticas")]
+    [SerializeField] private float velocidadMovimiento = 1f;
+    [SerializeField] private int vidasMaximas = 3;
     private int vidasActuales;
     private Rigidbody2D rb;
+    private Transform jugador;
 
     void Start()
     {
         vidasActuales = vidasMaximas;
+        jugador = GameObject.FindGameObjectWithTag("Jugador").transform;
         rb = GetComponent<Rigidbody2D>();
     }
-
+    void Update()
+    {
+        // Mover al enemigo hacia el jugador
+        if (jugador != null)
+        {
+            Vector2 direccion = jugador.position - transform.position;
+            direccion.Normalize();
+            transform.Translate(direccion * velocidadMovimiento * Time.deltaTime);
+        }
+    }
     public void RecibirGolpe()
     {
         vidasActuales--;
